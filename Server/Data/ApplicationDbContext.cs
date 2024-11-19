75ef6a2030e63272f999f681c5b11f58e7f9703d
Karin Åstrand <karinastrand@yahoo.se>
2024-11-19 17:52:38 +0100

    added db

---
+++ Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+
+namespace Server.Data;
+
+public class ApplicationDbContext:DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+    {
+        
+    }
+    public DbSet<Category> Categories  { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Category>().HasData(
+           new Category
+           {
+               CategoryId = 1,
+               ThumnailImagePath="uploads/placeholder.jpg",
+               Name="Kategori1",
+               Description="Beskrivning av kategori 1"
+           },
+            new Category
+            {
+                CategoryId = 2,
+                ThumnailImagePath = "uploads/placeholder.jpg",
+                Name = "Kategori1",
+                Description = "Beskrivning av kategori 2"
+            },
+             new Category
+             {
+                 CategoryId = 3,
+                 ThumnailImagePath = "uploads/placeholder.jpg",
+                 Name = "Kategori1",
+                 Description = "Beskrivning av kategori 3"
+             }
+        );
+    }
+}
