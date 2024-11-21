c48c658c3aae26cd41781eb8435c7fda61f0bff1
Karin Åstrand <karinastrand@yahoo.se>
2024-11-21 19:15:44 +0100

    .

---
+++ Client/Services/InMemoryDataBase.cs
@@ -0,0 +1,44 @@
+﻿using Client.Static;
+using Shared.Models;
+using System.Net.Http.Json;
+
+namespace Client.Services;
+
+internal sealed class InMemoryDataBase
+{
+    private readonly HttpClient _httpClient;
+
+    public InMemoryDataBase(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+
+    private List<Category> _categories = null;
+
+    internal List<Category> Categories
+    {
+        get { return _categories; }
+        set
+        {
+            _categories = value;
+            NotifyCategoriesDataChanged();
+        }
+    }
+
+    private bool _gettingCategoriesFromDatabaseAndInMemory = false;
+    internal async Task GetCategoriesFromDatabaseAndInMemory()
+    {
+        //Only allow one Get request to run at a time
+        if (_gettingCategoriesFromDatabaseAndInMemory==false)
+        {
+            _gettingCategoriesFromDatabaseAndInMemory=true;
+            _categories = await _httpClient.GetFromJsonAsync<List<Category>>(ApiEndpoints.s_categories);
+            _gettingCategoriesFromDatabaseAndInMemory = false;
+
+        }
+    }
+
+    internal event Action OnCategoriesDataChanged;
+    private void NotifyCategoriesDataChanged()=> OnCategoriesDataChanged?.Invoke();
+    
+}
---
+++ Client/Static/ApiEndpoints.cs
@@ -0,0 +1,14 @@
+﻿namespace Client.Static;
+
+internal static class ApiEndpoints
+{
+#if DEBUG
+    internal const string ServerBaseUrl = "https://localhost:7025";
+#else
+    internal const string ServerBaseUrl = "https://localhost:7025"; //change when published
+#endif
+
+    internal readonly static string s_categories =$"{ServerBaseUrl}";
+
+
+}
